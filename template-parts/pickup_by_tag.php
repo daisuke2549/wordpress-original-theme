d52572700805484fbfb07c0a12f5f0e462b0840a
daisuke sasaki <gerund25495641@gmail.com>
2021-02-19 06:34:08 +0900

    ピックアップ記事の表示の途中

---
+++ template-parts/pickup_by_tag.php
@@ -0,0 +1,36 @@
+    <!-- pickup_by_tag -->
+	<div id="pickup">
+		<div class="inner">
+			<div class="pickup-items">
+            <?php
+            $pickup_posts = get_posts(
+            array(
+            'post_type' => 'post', 
+            'posts_per_page' => '3',
+            'tag' => 'pickup', 
+            'orderby' => 'DESC'
+            )
+            );
+            ?>
+     <?php foreach ( $pickup_posts as $post ) : setup_postdata($post); ?>
+                <a href="<?php echo esc_url( get_permalink( $id ) ); ?>" class="pickup-item">
+                <div class="pickup-item-img">
+                <?php
+                if ( has_post_thumbnail( $id ) ) {
+                echo get_the_post_thumbnail( $id, 'large' );
+                } else {
+                echo '<img src="' . esc_url( get_template_directory_uri() ) . '/img/noimg.png" alt="">';
+                }
+                ?>
+                <div class="pickup-item-tag"><?php my_the_post_category( false, $id ); ?></div><!-- /pickup-item-tag -->
+                </div><!-- /pickup-item-img -->
+                <div class="pickup-item-body">
+                <h2 class="pickup-item-title"><?php echo esc_html( get_the_title( $id ) ); ?></h2><!-- /pickup-item-title -->
+                </div><!-- /pickup-item-body -->
+                </a><!-- /pickup-item -->
+
+                <?php endforeach; ?>
+			</div><!-- /pickup-items -->
+
+		</div><!-- /inner -->
+	</div><!-- /pickup_by_tag -->
